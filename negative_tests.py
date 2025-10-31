e0b38e01d4c74fa28e77f0d1bf288d883bfa687c
Тырыкина Полина <polinatyrykina@gmail.ru>
2025-10-31 16:12:50 +0700

    zzz

--- negative_tests.py
+++ negative_tests.py
@@ -5,15 +5,14 @@ from dis_1 import calculate_discriminant
 class TestDiscriminant(unittest.TestCase):
     
     def test_positive_discriminant(self):
-        self.assertEqual(calculate_discriminant(1,4,6),1)  
+        self.assertEqual(calculate_discriminant(1,-3,2),1)  
     
     def test_zero_discriminant(self):
-        self.assertEqual(calculate_discriminant(1,3, -2), 0)
+        self.assertEqual(calculate_discriminant(1, -2, 1), 0)
     
     def test_negative_discriminant(self):
-        self.assertEqual(calculate_discriminant(1,1,5),-16)  
+        self.assertEqual(calculate_discriminant(1,2,5),-16)  
 
 
 if __name__ == "__main__":
     unittest.main()
-
