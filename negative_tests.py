4b25be172b1bff6805d47b7ff474c7422c1f3c9f
Тырыкина Полина <polinatyrykina@gmail.ru>
2025-10-31 16:21:03 +0700

    zzzz

--- negative_tests.py
+++ negative_tests.py
@@ -3,12 +3,13 @@ from dis_1 import calculate_discriminant
 
 
 class TestDiscriminant(unittest.TestCase):
-    
+
     def test_positive_discriminant(self):
         self.assertEqual(calculate_discriminant(1, -3, 2), 1)  
-    
+
     def test_zero_discriminant(self):
         self.assertEqual(calculate_discriminant(1, -2, 1), 0)
+
     def test_negative_discriminant(self):
         self.assertEqual(calculate_discriminant(1, 2, 5), -16)  
 
